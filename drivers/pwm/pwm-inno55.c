0c480b2ad323ace1919c37b090e5f3fdef75a69a
sunjw <sunjw@innosilicon.com.cn>
2018-06-26 15:17:23 +0800

    Optiza pwm fan code

--- drivers/pwm/pwm-inno55.c
+++ drivers/pwm/pwm-inno55.c
@@ -20,44 +20,49 @@
 #include <linux/seq_file.h>
 
 #define MAGIC_NUMBER   'k'
-#define SET_FREQ_CMD0   _IO(MAGIC_NUMBER, 0)
-#define SET_DUTY_CMD0   _IO(MAGIC_NUMBER, 1)
-#define SET_FREQ_CMD1   _IO(MAGIC_NUMBER, 2)
-#define SET_DUTY_CMD1   _IO(MAGIC_NUMBER, 3)
+#define SET_FREQ_FAN0   _IO(MAGIC_NUMBER, 0)
+#define SET_DUTY_FAN0   _IO(MAGIC_NUMBER, 1)
+#define SET_FREQ_FAN1   _IO(MAGIC_NUMBER, 2)
+#define SET_DUTY_FAN1   _IO(MAGIC_NUMBER, 3)
 
 
-#define PWM_VERSION    "pwmdev: userspace pwm driver"
-#define CHRDEVNAME     "pwmdev_inno"
-#define PWM_MAJOR      0
+#define PWM_FAN_MAJOR      (0)
+#define PWM_FAN_DEVNAME    "pwm_fan_devnode"
+#define PWM_FAN_CLASSNAME  "pwm_fan_classnode"
+#define PWM_FAN_DRIVERNAME "pwmdrv_inno"
+#define PWM_FAN_PROCNAME   "pwm_fan_procname"
+//#define PROC_PWM_SIZE       128
 
-#define DRIVER_NAME_PWM    "pwmdrv_inno"
+#define PWM_FAN0_FREQ      "fan0_freq"
+#define PWM_FAN0_DUTY      "fan0_duty"
+#define PWM_FAN1_FREQ      "fan1_freq"
+#define PWM_FAN1_DUTY      "fan1_duty"
 
-#define PWM_BASEADDR   0x602f0000
-#define PWM_SIZE       0x10000
+struct class    *pwm_fan_class;
+static dev_t    pwm_fan_devno;
+struct device   *pwm_fan_device;
+static void     *pMemMapIO;
 
-
-struct class *pwmdev_class;
-static int pwm;
-struct device *pwmdev_device;
-static void *pMemMapIO;
-static dev_t pwm_devno;
+struct proc_dir_entry *mydir = NULL;
 
 
 static int nOpenFlag = 0;
 
 static int pwm_open(struct inode *inode, struct file *file)
 {
+    uint32_t RegValue0 = 0;
+    uint32_t RegValue1 = 0;
     if(nOpenFlag)
     {
         return -EBUSY;
     }
     nOpenFlag++;
-    
-    *((unsigned int *)(pMemMapIO + (0x1 << 2))) = 0x02;
+
+    *((unsigned int *)(pMemMapIO + (0x1 << 2))) = 0x2;
     *((unsigned int *)(pMemMapIO + (0x2 << 2))) = 0xff;
     
     try_module_get(THIS_MODULE);
-    printk("Open pwm node ok!\n");
+    printk("Open pwm fan node ok!\n");
     return 0;
 }
 
@@ -68,25 +73,25 @@ static int pwm_release(struct inode *inode, struct file *file)
     return 0;
 }
 
-static void set_pwm_freq_inno55_0(uint32_t RegValue)
+static void set_freq_fan0(uint32_t RegValue)
 {
     printk("pwm set Freq reg=0x%08x , value=0x%08x\n",(unsigned int)(pMemMapIO + (0x12 << 2)),RegValue);
     *((unsigned int *)(pMemMapIO + (0x12 << 2))) = RegValue;
 }
 
-static void set_pwm_duty_inno55_0(uint32_t RegValue)
+static void set_duty_fan0(uint32_t RegValue)
 {
     printk("pwm set Duty reg=0x%08x , value=0x%08x\n",(unsigned int)(pMemMapIO + (0x13 << 2)),RegValue);
     *((unsigned int *)(pMemMapIO + (0x13 << 2))) = RegValue;
 }
 
-static void set_pwm_freq_inno55_1(uint32_t RegValue)
+static void set_freq_fan1(uint32_t RegValue)
 {
     printk("pwm set Freq reg=0x%08x , value=0x%08x\n",(unsigned int)(pMemMapIO + (0x14 << 2)),RegValue);
     *((unsigned int *)(pMemMapIO + (0x14 << 2))) = RegValue;
 }
 
-static void set_pwm_duty_inno55_1(uint32_t RegValue)
+static void set_duty_fan1(uint32_t RegValue)
 {
     printk("pwm set Duty reg=0x%08x , value=0x%08x\n",(unsigned int)(pMemMapIO + (0x15 << 2)),RegValue);
      *((unsigned int *)(pMemMapIO + (0x15 << 2))) = RegValue;
@@ -96,21 +101,21 @@ static int pwm_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
 {
     switch(cmd)
     {
-        case SET_FREQ_CMD0:
-            printk("start set SET_FREQ_CMD0!\n");
-            set_pwm_freq_inno55_0(arg);
+        case SET_FREQ_FAN0:
+            printk("start set fan 0 freq!\n");
+            set_freq_fan0(arg);
             break;
-        case SET_DUTY_CMD0:
-            printk("start set SET_DUTY_CMD0!\n");
-            set_pwm_duty_inno55_0(arg);
+        case SET_DUTY_FAN0:
+            printk("start set fan 0 duty!\n");
+            set_duty_fan0(arg);
             break;
-        case SET_FREQ_CMD1:
-            printk("start set SET_FREQ_CMD1!\n");
-            set_pwm_freq_inno55_1(arg);
+        case SET_FREQ_FAN1:
+            printk("start set fan 1 freq!\n");
+            set_freq_fan1(arg);
             break;
-        case SET_DUTY_CMD1:
-            printk("start set SET_DUTY_CMD1!\n");
-            set_pwm_duty_inno55_1(arg);
+        case SET_DUTY_FAN1:
+            printk("start set fan 1 duty!\n");
+            set_duty_fan1(arg);
             break;
         default:
             break;
@@ -119,130 +124,56 @@ static int pwm_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
     return 0 ;
 }
 
-struct file_operations pwm_fops = {
+struct file_operations pwm_fan_fops = {
         .owner  = THIS_MODULE,
         .open   = pwm_open,
         .unlocked_ioctl = pwm_ioctl,
         .release= pwm_release,
 };
 
-/*
-struct device {
-    
-};
-
-struct platform_device pwm_device={
-    const char *name;
-    u32 id;
-    struct device dev;
-    u32 num_resourcesl
-    struct resource *resource;
-};
-*/
-
-/*static irqreturn_t pwm_irq_function(int irq,void *dev_id)
-{
-	printk("pwm interrupt ok\n");
-	return IRQ_HANDLED;
-}*/
-
-
+struct resource *pwm_fan_mem = NULL;
 static int pwm_device_probe(struct platform_device *pdev)
 {
-    //struct resource *pwm_irq;
-    struct resource *pwm_mem;
-    //struct device *dev = &pdev->dev;
+    struct resource *pwm_fan_res = NULL;
     
-    struct resource *res = NULL;
-    
-    pwm_mem = platform_get_resource(pdev, IORESOURCE_MEM, 0);
-    if(!pwm_mem)
+    pwm_fan_mem = platform_get_resource(pdev, IORESOURCE_MEM, 0);
+    if(!pwm_fan_mem)
     {
         printk("platform get resource of mem Failed!\n");
         return -1;
     }
 
-    printk("platform_get_resource of mem success!\n");
+    printk("platform_get_resource of fan mem success!\n");
 
-    printk("start=0x%08x,size=%d\n",pwm_mem->start,pwm_mem->end - pwm_mem->start + 1);
+    printk("start=0x%08x,size=%d\n",pwm_fan_mem->start,pwm_fan_mem->end - pwm_fan_mem->start + 1);
     
-    res = request_mem_region(pwm_mem->start, pwm_mem->end - pwm_mem->start + 1, DRIVER_NAME_PWM);
-    if(!res)
+    pwm_fan_res = request_mem_region(pwm_fan_mem->start, pwm_fan_mem->end - pwm_fan_mem->start + 1, PWM_FAN_DRIVERNAME);
+    if(!pwm_fan_res)
     {
         printk("request mem Failed!\n");
         return -1;
     }
     printk("request_mem_region of mem success!\n");
 
-    //void __iomem *baseaddr;
-
-    pMemMapIO = ioremap(pwm_mem->start, pwm_mem->end - pwm_mem->start + 1);
+    pMemMapIO = ioremap(pwm_fan_mem->start, pwm_fan_mem->end - pwm_fan_mem->start + 1);
     printk("BaseAddr:0x%08x\n",(unsigned int __force)pMemMapIO);
-
-    #if 0
-    pwm_irq = platform_get_resource(pdev, IORESOURCE_IRQ, 0);
-    if(!pwm_irq)
-    {
-        printk("platform get resource of irq Failed!\n");
-        return -1;
-    }
-
-    printk("platform_get_resource of irq success!\n");
-    printk("start=%d\n",pwm_irq->start);
-    res = request_irq(pwm_irq->start, &pwm_irq_function, 0, DRIVER_NAME_PWM, NULL);
-    if(!res)
-    {
-        printk("request irq Failed!\n");
-        return -1;
-    }
-    printk("request_irq of irq success!\n");
-    #endif
     
     return 0;
 }
 
-#define PROC_PWM_SIZE   128
-#define PROC_PWM_NAME   "pwm55info"
 
-
-static int pwm_stop(struct seq_file *m,void *p)
-{
+static int proc_fan0_freq_open(struct inode *inode,struct file  *file)
+{  
     return 0;
 }
 
-static int pwm_start(struct seq_file *m,loff_t *pos)
+static int proc_fan0_freq_read(struct inode *inode,struct file  *file)
 {
-    return *pos < 1?(void *)1:NULL;
-}
-
-
-static struct seq_operations  proc_pwm_seq_ops = {
-    .start = pwm_start,
-    .stop  = pwm_stop
-};
-
-static void proc_pwm_open(struct inode *inode,struct file *file)
-{
-    int ret = 0;
-    struct seq_file *m;
-
-    ret = seq_open(file,&proc_pwm_seq_ops);
-    m = file->private_data;
-    m->private = file->f_path.dentry->d_iname;
-
-    return ;
-    
-}
-
-static int proc_pwm_read(struct seq_file *m,void *p)
-{
-    printk("proc_pwm_read reg addr:0x%08x, value: 0x%08x\n",((unsigned int )(pMemMapIO + (0x12 << 2))),*((unsigned int *)(pMemMapIO + (0x13 << 2))));
-    printk("proc_pwm_read reg addr:0x%08x, value: 0x%08x\n",((unsigned int )(pMemMapIO + (0x14 << 2))),*((unsigned int *)(pMemMapIO + (0x15 << 2))));
-    
+    printk("0x%08x\n",*((unsigned int *)(pMemMapIO + (0x12 << 2))));
     return 0;
 }
 
-static int proc_pwm_write(struct file *file,const char __user *buffer,size_t count,loff_t *offp)
+static int proc_fan0_freq_write(struct file *file,const char __user *buffer,size_t count,loff_t *offp)
 {
     uint32_t value = 0;
     char reg[128] = {0};
@@ -252,8 +183,68 @@ static int proc_pwm_write(struct file *file,const char __user *buffer,size_t cou
     }
     else
     {
-        printk("proc_pwm_write reg=%s\n",reg);
-    
+        if(sscanf(reg,"%d",&value) == 1)
+        {
+            printk("echo value=0x%08x\n",value);
+            *((unsigned int *)(pMemMapIO + (0x12 << 2))) = value;
+        }
+    }
+
+    return count;
+}
+
+static int proc_fan0_duty_open(struct inode *inode,struct file  *file)
+{  
+    return 0;
+}
+
+static int proc_fan0_duty_read(struct inode *inode,struct file  *file)
+{
+    printk("0x%08x\n",*((unsigned int *)(pMemMapIO + (0x13 << 2))));
+    return 0;
+}
+
+static int proc_fan0_duty_write(struct file *file,const char __user *buffer,size_t count,loff_t *offp)
+{
+    uint32_t value = 0;
+    char reg[128] = {0};
+    if(copy_from_user((void *)reg,buffer,count))
+    {
+        printk("copy from user data Failed!\n");
+    }
+    else
+    {
+        if(sscanf(reg,"%d",&value) == 1)
+        {
+            printk("echo value=0x%08x\n",value);
+            *((unsigned int *)(pMemMapIO + (0x13 << 2))) = value;
+        }
+    }
+
+    return count;
+}
+
+static int proc_fan1_freq_open(struct inode *inode,struct file  *file)
+{  
+    return 0;
+}
+
+static int proc_fan1_freq_read(struct inode *inode,struct file  *file)
+{
+    printk("0x%08x\n",*((unsigned int *)(pMemMapIO + (0x14 << 2))));
+    return 0;
+}
+
+static int proc_fan1_freq_write(struct file *file,const char __user *buffer,size_t count,loff_t *offp)
+{
+    uint32_t value = 0;
+    char reg[128] = {0};
+    if(copy_from_user((void *)reg,buffer,count))
+    {
+        printk("copy from user data Failed!\n");
+    }
+    else
+    { 
         if(sscanf(reg,"%d",&value) == 1)
         {
             printk("echo value=0x%08x\n",value);
@@ -264,92 +255,149 @@ static int proc_pwm_write(struct file *file,const char __user *buffer,size_t cou
     return count;
 }
 
-static struct file_operations proc_pwm_fops = {
-    .open  = proc_pwm_open,
-    .read  = proc_pwm_read,
-    .write = proc_pwm_write,
-    //.realse= proc_pwm_release,
-};
+static int proc_fan1_duty_open(struct inode *inode,struct file  *file)
+{  
+    return 0;
+}
 
-static int create_proc_pwm_inode(char *name)
+static int proc_fan1_duty_read(struct inode *inode,struct file  *file)
 {
-    struct proc_dir_entry *pwm_proc_entry;
-    u8 proc_pwm_name[50] = {0};
-    if(PROC_PWM_NAME == NULL)
+    printk("0x%08x\n",*((unsigned int *)(pMemMapIO + (0x15 << 2))));
+    return 0;
+}
+
+static int proc_fan1_duty_write(struct file *file,const char __user *buffer,size_t count,loff_t *offp)
+{
+    uint32_t value = 0;
+    char reg[128] = {0};
+    if(copy_from_user((void *)reg,buffer,count))
     {
-        sprintf(proc_pwm_name,"%s",name);
+        printk("copy from user data Failed!\n");
     }
     else
     {
-        sprintf(proc_pwm_name,"%s/%s",PROC_PWM_NAME,name);
+        if(sscanf(reg,"%d",&value) == 1)
+        {
+            printk("echo value=0x%08x\n",value);
+            *((unsigned int *)(pMemMapIO + (0x15 << 2))) = value;
+        }
     }
 
-    pwm_proc_entry = proc_create(proc_pwm_name,0644,NULL,&proc_pwm_fops);
-    if(NULL == pwm_proc_entry)
-    {
-        remove_proc_entry(proc_pwm_name,NULL);
-        printk("Initialize /proc/%s Failed!\n",proc_pwm_name);
-        return -1;
-    }
-
-    return 0;
+    return count;
 }
-static int create_proc_pwm_file(void)
-{
-    struct proc_dir_entry *mydir = NULL;
 
-    mydir = proc_mkdir(PROC_PWM_NAME,NULL);
+static struct file_operations proc_pwm_fan0_freq_fops = {
+    .open  = proc_fan0_freq_open,
+    .read  = proc_fan0_freq_read,
+    .write = proc_fan0_freq_write,
+};
+static struct file_operations proc_pwm_fan0_duty_fops = {
+    .open  = proc_fan0_duty_open,
+    .read  = proc_fan0_duty_read,
+    .write = proc_fan0_duty_write,
+};
+static struct file_operations proc_pwm_fan1_freq_fops = {
+    .open  = proc_fan1_freq_open,
+    .read  = proc_fan1_freq_read,
+    .write = proc_fan1_freq_write,
+};
+static struct file_operations proc_pwm_fan1_duty_fops = {
+    .open  = proc_fan1_duty_open,
+    .read  = proc_fan1_duty_read,
+    .write = proc_fan1_duty_write,
+};
+
+static int create_proc_node_pwm(void)
+{
+    struct proc_dir_entry *proc_entry_fan0_freq;
+    struct proc_dir_entry *proc_entry_fan0_duty;
+    struct proc_dir_entry *proc_entry_fan1_freq;
+    struct proc_dir_entry *proc_entry_fan1_duty;
+
+    mydir = proc_mkdir(PWM_FAN_PROCNAME,NULL);
 
     if(!mydir)
     {
-        printk("Create /proc/%s Failed!\n",PROC_PWM_NAME);
+        printk("Create /proc/%s Failed!\n",PWM_FAN_PROCNAME);
         return -1;
     }
     
-    printk("Create /proc/%s Success!\n",PROC_PWM_NAME);
+    printk("Create /proc/%s Success!\n",PWM_FAN_PROCNAME);
+
+    proc_entry_fan0_freq = proc_create(PWM_FAN0_FREQ,0644,mydir,&proc_pwm_fan0_freq_fops);
+    if(NULL == proc_entry_fan0_freq)
+    {
+        remove_proc_entry(PWM_FAN_PROCNAME,NULL);
+        printk("Initialize /proc/%s Failed!\n",PWM_FAN_PROCNAME);
+        goto error_exit;
+    }
+
+    proc_entry_fan0_duty = proc_create(PWM_FAN0_DUTY,0644,mydir,&proc_pwm_fan0_duty_fops);
+    if(NULL == proc_entry_fan0_duty)
+    {
+        remove_proc_entry(PWM_FAN_PROCNAME,NULL);
+        printk("Initialize /proc/%s Failed!\n",PWM_FAN_PROCNAME);
+        goto error_exit;
+    }
+
+    proc_entry_fan1_freq = proc_create(PWM_FAN1_FREQ,0644,mydir,&proc_pwm_fan1_freq_fops);
+    if(NULL == proc_entry_fan1_freq)
+    {
+        remove_proc_entry(PWM_FAN_PROCNAME,NULL);
+        printk("Initialize /proc/%s Failed!\n",PWM_FAN_PROCNAME);
+        goto error_exit;
+    }
+
+    proc_entry_fan1_duty = proc_create(PWM_FAN1_DUTY,0644,mydir,&proc_pwm_fan1_duty_fops);
+    if(NULL == proc_entry_fan1_duty)
+    {
+        remove_proc_entry(PWM_FAN_PROCNAME,NULL);
+        printk("Initialize /proc/%s Failed!\n",PWM_FAN_PROCNAME);
+        goto error_exit;
+    }
+
     return 0;
-}
-
-
-static void create_proc_node_pwm(void)
-{
-    create_proc_pwm_file();
-    create_proc_pwm_inode("pwm55");
+    
+error_exit:
+    remove_proc_entry(PWM_FAN_PROCNAME,mydir);  
+    return -1;
 }
 
 
 static int pwm_55_probe(struct platform_device *pdev)
 {
-    pwm = register_chrdev(PWM_MAJOR, CHRDEVNAME, &pwm_fops);
+    int pwm = 0;
+    pwm = register_chrdev(PWM_FAN_MAJOR, PWM_FAN_DEVNAME, &pwm_fan_fops);
     
 	if (pwm < 0) 
     {
-		printk (KERN_WARNING CHRDEVNAME ": unable to get major %d\n",PWM_MAJOR);
-		unregister_chrdev(PWM_MAJOR, CHRDEVNAME);
+		printk (KERN_WARNING PWM_FAN_DEVNAME ": unable to get major %d\n",PWM_FAN_MAJOR);
+		unregister_chrdev(PWM_FAN_MAJOR, PWM_FAN_DEVNAME);
         return -1;
 	}
 
-    printk("register_chrdev Success\n");
+    printk("register pwm fan chrdev success!\n");
     
-	pwmdev_class = class_create(THIS_MODULE, CHRDEVNAME);
-	if (IS_ERR(pwmdev_class)) {
-		class_destroy(pwmdev_class);
-        return -1;
-	}
-
-    printk("class create Success\n");
-    
-    pwm_devno = MKDEV(pwm,0);
-    
-    pwmdev_device = device_create(pwmdev_class, NULL, pwm_devno, NULL, CHRDEVNAME);
-    if (IS_ERR(pwmdev_device))
+	pwm_fan_class = class_create(THIS_MODULE, PWM_FAN_CLASSNAME);
+	if (IS_ERR(pwm_fan_class)) 
     {
-        class_destroy(pwmdev_class);
-        unregister_chrdev(PWM_MAJOR, CHRDEVNAME);
+		class_destroy(pwm_fan_class);
+        return -1;
+	}
+
+    printk("class pwm fan node create success!\n");
+    
+    pwm_fan_devno = MKDEV(pwm,0);
+    
+    pwm_fan_device = device_create(pwm_fan_class, NULL, pwm_fan_devno, NULL, PWM_FAN_DEVNAME);
+    if (IS_ERR(pwm_fan_device))
+    {
+        class_destroy(pwm_fan_device);
+        unregister_chrdev(PWM_FAN_MAJOR, PWM_FAN_DEVNAME);
         return -1;
     }
     
-    printk("device create Success\n");
+    printk("bind dev and class node success!\n");
     
     pwm_device_probe(pdev);
 
@@ -358,36 +406,25 @@ static int pwm_55_probe(struct platform_device *pdev)
     return 0;
 }
 
-static void remove_proc_node_pwm(char *name)
+static void remove_proc_node(void)
 {
-    u8 proc_name[50] = {0};
-
-    if(PROC_PWM_NAME == NULL)
-    {
-        sprintf(proc_name,"%s",name);
-    }
-    else
-    {
-        sprintf(proc_name,"%s/%s",PROC_PWM_NAME,name);
-    }
-
-    remove_proc_entry(proc_name,NULL);
-    
-    if(PROC_PWM_NAME != NULL)
-    {
-        remove_proc_entry(PROC_PWM_NAME,NULL);
-    }
-    
+    remove_proc_entry(PWM_FAN0_FREQ,mydir); 
+    remove_proc_entry(PWM_FAN0_DUTY,mydir); 
+    remove_proc_entry(PWM_FAN1_FREQ,mydir); 
+    remove_proc_entry(PWM_FAN1_DUTY,mydir); 
+    remove_proc_entry(PWM_FAN_PROCNAME,NULL);  
 }
 
 static int pwm_55_remove(struct platform_device *pdev)
 {
     printk("rmmod pwm module!\n");
-    remove_proc_node_pwm("pwm55");
+    remove_proc_node();   
     iounmap(pMemMapIO);
-    device_destroy(pwmdev_class,MKDEV(pwm,0));
-	class_destroy(pwmdev_class);
-	unregister_chrdev (PWM_MAJOR, CHRDEVNAME);
+    release_mem_region(pwm_fan_mem->start, pwm_fan_mem->end - pwm_fan_mem->start + 1);
+    pwm_fan_mem = NULL;
+    device_destroy(pwm_fan_class,pwm_fan_devno);
+	class_destroy(pwm_fan_class);
+	unregister_chrdev (PWM_FAN_MAJOR, PWM_FAN_DEVNAME);
     
     return 0;
 }
@@ -395,7 +432,7 @@ static int pwm_55_remove(struct platform_device *pdev)
 
 static struct platform_driver pwm_driver = {
         .driver= {
-            .name = DRIVER_NAME_PWM,
+            .name = PWM_FAN_DRIVERNAME,
          },
         .probe       = pwm_55_probe,
         .remove      = pwm_55_remove,
@@ -403,13 +440,13 @@ static struct platform_driver pwm_driver = {
 
 static int __init pwmdev_init(void)
 {
-    printk("pwm init...(%s) \n",PWM_VERSION);
+    printk("pwm fan init...\n");
 	return platform_driver_register(&pwm_driver);
 }
 
 static void __exit pwmdev_exit(void)
 {
-    printk("pwm exit...\n");
+    printk("pwm fan exit...\n");
     platform_driver_unregister(&pwm_driver);
 }
 
